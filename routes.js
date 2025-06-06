bad6ee3f0695bf5b8e2eafb8e35e0e70b86c4608
Syeda <zainab.17703@gmail.com>
2025-06-06 20:09:18 +0530

    UpdatedRoutes

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname,'..'));
+app.use(express.static(path.join(__dirname,'..')));
 
 //serves each HTML file separately.
 app.get('/pricing',(req,res)=>{
