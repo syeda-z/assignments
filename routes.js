53444a7d6b82054e0b879b3f772aa5fa62c21d1d
Syeda <zainab.17703@gmail.com>
2025-06-06 20:01:29 +0530

    Accepted

---
+++ routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname,'..'));
+
+//serves each HTML file separately.
+app.get('/pricing',(req,res)=>{
+    res.sendFile(path.join(__dirname,'assignment1.html'));
+});
+
+app.get('/form',(req,res)=>{
+    res.sendFile(path.join(__dirname,'assignment2.html'));
+});
+
+app.get('/album',(req,res)=>{
+    res.sendFile(path.join(__dirname,'assignment3.html'));
+});
+
+app.listen(3000,()=>{
+    console.log('server is up and running at port 3000');
+});
