ddc1a0fc778b43173bb08ca94ef5e364250d2083
Syeda <zainab.17703@gmail.com>
2025-06-06 12:30:02 +0530

    Accepted

---
+++ assignment3.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static(path.join(__dirname,'..')));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'assignment3.html'));
+});
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    //database work here
+    res.send('seccuessfully posted data');
+});
+
+app.listen(3000);
